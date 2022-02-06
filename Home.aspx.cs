2c77b90b0d642968df19fe2211ba32577a233082
liormatana <99083726+liormatana@users.noreply.github.com>
2022-02-06 18:23:53 +0200

    Update and rename בית.aspx.cs to Home.aspx.cs

--- Home.aspx.cs
+++ Home.aspx.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class בית : System.Web.UI.Page
+public partial class Home : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
